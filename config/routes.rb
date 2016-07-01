5d41885ec8c4c269a061e9c50a4dcbd60042f170
psswf1172 <jennifermabella@gmail.com>
2016-07-01 12:39:44 -0400

    show action on users

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,18 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
-  scope '/admin' do 
-    resources :users
-  end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show]
+    end
 
-  # You can have the root of your site routed with "root"
+
   root 'home#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  namespace :api, defaults: { format: :json },
-    constraints: { subdomain: 'api' }, path: '/' do
-    
-    scope module: :v1, 
-      constraints: ApiConstraints.new(version: 1, default: true) do
-
-    end
   end
 end

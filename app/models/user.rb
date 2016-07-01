5a0c693c91deed9385bb87c78e316252fbceb73a
psswf1172 <jennifermabella@gmail.com>
2016-07-01 13:29:20 -0400

    seeded db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # has_secure_password
+  # before_save :verify_authentication_token
+
+  # def self.authenticate_user(params)
+  #   user = self.where("email = ?", params[:email]).any?
+  #   user if user && user.authenticate!(params[:password])
+  # end
+
+  # private
+
+  # def verify_authentication_token
+  #   if authentication_token.blank?
+  #     self.authentication_token = generate_auth_token
+  #   end
+  # end
+
+  #   def generate_auth_token
+  #     loop do
+  #       token = SecureRandom.urlsafe_base64(15)
+  #       break token unless User.where(authentication_token: token).first
+  #   end
+  # end
+
 end

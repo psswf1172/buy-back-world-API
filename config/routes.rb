8b53ad4db8182b94482785f49428c63d4a15803a
psswf1172 <jennifermabella@gmail.com>
2016-07-01 11:56:36 -0400

    added first user specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
+  scope '/admin' do 
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

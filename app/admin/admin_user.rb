ed2568b9c5869217f2e679ce12134ed6b90aedfd
espa77 <ethansteiner@gmail.com>
2016-09-12 13:59:32 -0400

    text mailer styling

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :text_mail, :password, :password_confirmation
 
   index do
     selectable_column
     id_column
     column :email
+    column :text_mail
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
